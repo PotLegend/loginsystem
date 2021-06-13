9cf8453f58497cc72febc17d00cff7eec05dd058
PotLegend <80139073+PotLegend@users.noreply.github.com>
2021-06-13 13:39:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -32,4 +32,4 @@ app.get('/', (req, res) =>{
     res.render('base', { title : "Login System"});
 })
 
-app.listen(port, ()=>{ console.log("Lostening to the server on http://localhost:3000")});
+app.listen(port, ()=>{ console.log("Listening to the server on http://localhost:3000")});
